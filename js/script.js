3b9d8501853591c74b8e42b7198dd735e4988bb4
Serhii Khomenko <cahxoce2101@gmail.com>
2019-08-21 14:55:35 +0300

    +slick

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
+window.onload = () => {
+     let x = document.querySelector("#nav");
+     document.querySelector("#burger").onclick = function () {
+          x.classList.toggle('responsive');
+     };
+};
 
 
 
-
-window.onload = ()=>{
-      let x = document.querySelector("#nav");
-      document.querySelector("#burger").onclick = function(){
-           x.classList.toggle('responsive');
-      };
-    };
-
+$('.slider').slick({
+     slidesToShow: 1,
+     dots: true,
+     responsive: [{
+          breakpoint: 768,
+          settings: {
+               slidesToShow: 1,
+               arrows: false
+          }
+     }]
+});
