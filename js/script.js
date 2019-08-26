a864fdacb24907ae6f2d086fb94f338280853eec
Serhii Khomenko <cahxoce2101@gmail.com>
2019-08-26 19:55:12 +0300

    responsive

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+
+
 window.onload = () => {
      let x = document.querySelector("#nav");
-     document.querySelector("#burger").onclick = function () {
+    document.querySelector("#burger").onclick = function () {
           x.classList.toggle('responsive');
      };
 };
-
 
 
 $('.slider').slick({
